7ff5e4eb9b6016adf763642f336c3c3e443da735
Madtore <73850831+Madtore@users.noreply.github.com>
2024-10-19 12:01:44 +0200

    [FEATURES] add repository all models

---
+++ src/main/java/com/cesur/general/general/repositories/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.cesur.general.general.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cesur.general.general.models.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
