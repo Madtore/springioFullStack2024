3dc7ea8ed3337ff904e49d5e96e2f0c0c86547f7
Madtore <73850831+Madtore@users.noreply.github.com>
2024-10-19 11:21:58 +0200

    [FEATURES] create model AttachedFile, Chat, Comment and History

---
+++ src/main/java/com/cesur/general/general/models/Comment.java
@@ -0,0 +1,41 @@
+package com.cesur.general.general.models;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "createAt")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userCreated;
+
+    @ManyToOne
+    @JoinColumn(name = "incidence_id")
+    private Incidence incidence;
+
+}
